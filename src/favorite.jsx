3ca67f6d140300d806dfc30f0fbe2baae5a02228
Seth <sethyuan@gmail.com>
2023-03-07 15:58:48 +0800

    feat: left sidebar slider

--- src/favorite.jsx
+++ src/favorite.jsx
@@ -1,6 +1,9 @@
 import { render } from "preact"
+import { throttle } from "rambdax"
 import FavList from "./comps/FavList"
 import { hash, queryForSubItems } from "./libs/utils"
+
+let dragHandle = null
 
 export async function load() {
   logseq.provideStyle({
@@ -54,6 +57,27 @@ export async function load() {
         white-space: nowrap;
         text-overflow: ellipsis;
       }
+
+      .kef-ae-drag-handle {
+        content: "";
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        width: 4px;
+        z-index: 10;
+      }
+      .kef-ae-drag-handle:hover,
+      .kef-ae-dragging .kef-ae-drag-handle {
+        cursor: col-resize;
+        background: var(--ls-active-primary-color);
+      }
+      .kef-ae-dragging {
+        cursor: col-resize;
+      }
+      .kef-ae-dragging :is(#left-sidebar, #main-content-container) {
+        pointer-events: none;
+      }
     `,
   })
 
@@ -84,11 +108,34 @@ export async function load() {
   await processFavorites()
   await processRecents()
 
+  const graph = await logseq.App.getCurrentGraph()
+  const storedWidth = parent.localStorage.getItem(`kef-ae-lsw-${graph.name}`)
+  if (storedWidth) {
+    parent.document.documentElement.style.setProperty(
+      "--ls-left-sidebar-width",
+      `${+storedWidth}px`,
+    )
+  }
+
+  logseq.provideUI({
+    key: "kef-ae-drag-handle",
+    path: "#left-sidebar",
+    template: `<div class="kef-ae-drag-handle"></div>`,
+  })
+  setTimeout(() => {
+    dragHandle = parent.document.querySelector(
+      "#left-sidebar .kef-ae-drag-handle",
+    )
+    dragHandle.addEventListener("pointerdown", onPointerDown)
+  }, 0)
+
   // cleaning
   return () => {
     sidebarObserver.disconnect()
     favoritesObserver.disconnect()
     recentsObserver.disconnect()
+
+    dragHandle?.removeEventListener("pointerdown", onPointerDown)
   }
 }
 
@@ -141,3 +188,41 @@ function renderList(key, items, arrowContainer, fav) {
   const el = parent.document.getElementById(key)
   render(<FavList items={items} arrowContainer={arrowContainer} />, el)
 }
+
+function onPointerDown(e) {
+  e.preventDefault()
+  parent.document.documentElement.classList.add("kef-ae-dragging")
+  parent.document.addEventListener("pointermove", onPointerMove)
+  parent.document.addEventListener("pointerup", onPointerUp)
+  parent.document.addEventListener("pointercancel", onPointerUp)
+}
+
+function onPointerUp(e) {
+  e.preventDefault()
+  parent.document.removeEventListener("pointermove", onPointerMove)
+  parent.document.removeEventListener("pointerup", onPointerUp)
+  parent.document.removeEventListener("pointercancel", onPointerUp)
+  parent.document.documentElement.classList.remove("kef-ae-dragging")
+
+  const pos = e.clientX
+  parent.document.documentElement.style.setProperty(
+    "--ls-left-sidebar-width",
+    `${pos}px`,
+  )
+  ;(async () => {
+    const graph = await logseq.App.getCurrentGraph()
+    parent.localStorage.setItem(`kef-ae-lsw-${graph.name}`, pos)
+  })()
+}
+
+function onPointerMove(e) {
+  e.preventDefault()
+  move(e.clientX)
+}
+
+const move = throttle((pos) => {
+  parent.document.documentElement.style.setProperty(
+    "--ls-left-sidebar-width",
+    `${pos}px`,
+  )
+}, 12)
